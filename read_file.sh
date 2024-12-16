d14c24bb8a55529d29f5fffbbaa34c6d5d69d3d9
Roger Magnesved <roger.magnesved@gmail.com>
2024-12-16 13:32:39 +0100

    Handle direct copy from Google Doc in shell wrapper

--- read_file.sh
+++ read_file.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+# Wrapper script that handles cell copy from Google doc to a text file
+# and transforms the text to be able to feed it into image generator
+
 _file="$1"
 
 if [[ ! -f "${_file}" ]]; then
@@ -7,10 +10,10 @@ if [[ ! -f "${_file}" ]]; then
     exit 1
 fi
 
-for l in $(cat "${_file}" | tr -d ' '); do
+for l in $(cat "${_file}" | tr '\t' ';' | tr ',' ';' | tr -d ' '); do
     date=$(echo "${l}" | cut -d';' -f1)
-    level=$(echo "${l}" | cut -d';' -f2)
-    gender=$(echo "${l}" | cut -d';' -f3)
+    level=$(echo "${l}" | cut -d';' -f3 | grep -Eo '[[:digit:]]')
+    gender=$(echo "${l}" | cut -d';' -f4 | tr '[[:upper:]]' '[[:lower:]]')
 
     ./tournament-generator.py --date "${date}" --level "${level}" --gender "${gender}"
 
